44365c27a8231dda1c947634c280d6b31b3ca437
Artur Malabarba <bruce.connor.am@gmail.com>
2015-01-09 18:15:30 -0200

    Define tests

---
+++ test/paradox-test.el
@@ -0,0 +1,37 @@
+
+(require 'ert)
+(require 'paradox)
+
+(ert-deftest message ()
+  ""
+  (should
+   (string=
+    (paradox--format-message 'question '(1) '(1 2))
+    "Install 1 package, and Delete 2 packages? "))
+  (should
+   (string=
+    (paradox--format-message 'question nil '(1 2))
+    "Delete 2 packages? "))
+  (should
+   (string=
+    (paradox--format-message 'question '(1) nil)
+    "Install 1 package? "))
+  (should
+   (string=
+    (paradox--format-message nil '(1) '(1 2))
+    "Installed 1 package, and Deleted 2 packages."))
+  (should
+   (string=
+    (paradox--format-message nil nil '(1 2))
+    "Deleted 2 packages."))
+  (should
+   (string=
+    (paradox--format-message nil '(1) nil)
+    "Installed 1 package.")))
+
+(ert-deftest sanity ()
+  ""
+  (let ((paradox-github-token t))
+    (should (paradox-list-packages)))
+  (let ((paradox-github-token "okokok"))
+    (should-error (paradox-list-packages))))

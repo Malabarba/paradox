fdb5e78e6b3435dc969128ea292b0ed5a13344be
Artur Malabarba <bruce.connor.am@gmail.com>
2015-03-08 11:30:45 -0300

    Fix run.sh

--- helpers/run.sh
+++ helpers/run.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-[ -z "$EMACS" ] && echo "NO EMACS!!" && return
-
-# Paradox
-cd ~/.melpa &&
-    git pull &&
-    cd ~/.paradox/helpers &&
-    git checkout data &&
-    git pull &&
-    $EMACS --batch -Q -L . -L .. -l paradox-counter.el -f paradox-generate-star-count &&
-    git add .. &&
-    git commit -m "$(date)" &&
-    git push -v origin data:refs/heads/data &&
-    
-    
+if [[ -z "$EMACS" ]]; then
+    echo "NO EMACS!!";
+    exit 1;
+else
+    # Paradox
+    cd ~/.melpa &&
+        git pull &&
+        cd ~/.paradox/helpers &&
+        git checkout data &&
+        git pull &&
+        nice $EMACS --batch -Q -L . -L .. -l paradox-counter.el -f toggle-debug-on-error -f paradox-generate-star-count &&
+        git add .. &&
+        git commit -m "$(date)" &&
+        git push -v origin data:refs/heads/data;
+fi

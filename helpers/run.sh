240b610587c14fd08995975dcb08a86c2661864f
Artur Malabarba <bruce.connor.am@gmail.com>
2017-09-15 21:48:24 -0300

    Remove useless lines from run.sh

--- helpers/run.sh
+++ helpers/run.sh
@@ -5,13 +5,14 @@ if [[ -z "$EMACS" ]]; then
     exit 1;
 else
     # Paradox
-    cd ~/.melpa &&
-        git pull &> /dev/null &&
-        cd ~/.paradox &&
+    cd ~/.paradox &&
         git checkout data &> /dev/null &&
         git pull &> /dev/null &&
         cd ~/.paradox/helpers &&
-        /usr/bin/nice $EMACS --batch -Q -L . -L .. -l paradox-counter.el --eval '(setq debug-on-error t)' -f paradox-generate-star-count &&
+        /usr/bin/nice $EMACS --batch -Q \
+                      -L . -L .. -l paradox-counter.el \
+                      --eval '(setq debug-on-error t)' \
+                      -f paradox-generate-star-count &&
         git add .. &> /dev/null &&
         git commit -m "$(date)" &> /dev/null &&
         git push -v origin data:refs/heads/data &> /dev/null;

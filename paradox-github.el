4c3069bc0bb652efed72fea5f88ba9c37bbb5c23
R Primus <@users.noreply.github.com>
2018-08-07 19:45:44 +0100

    * paradox-github.el	(paradox--github-parse-response-code): Process 301 response code

--- paradox-github.el
+++ paradox-github.el
@@ -225,7 +225,9 @@ Leave point at the return code on the first line."
     (`200 t)   ;; OK, with content.
     ;; I'll implement redirection if anyone ever reports this.
     ;; For now, I haven't found a place where it's used.
-    ((or `301 `302 `303 `304 `305 `306 `307)
+    (`301
+     (search-forward "HTTP/1.1 ") t)
+    ((or `302 `303 `304 `305 `306 `307)
      (paradox--github-report "Redirect received:\n\n" (buffer-string))
      ;; (message "Received a redirect reply, please file a bug report (M-x `paradox-bug-report')")
      nil)
